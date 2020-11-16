c7784575ef53a7f012ca1263a93c43f976f29b72
chenyiming365 <73202054+chenyiming365@users.noreply.github.com>
2020-11-16 19:29:07 +0800

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,35 @@
+function elementClicked(element){
+	alert("你点击了 "+element.name_chs+".\n"+JSON.stringify(element)+"\n\n函数elementClicked(element)决定点击后会发生什么")
+}
+function getElementDemo(){
+	var value=document.getElementById("getElement").value
+	if(value!=""){
+		alert(JSON.stringify(getElement(value)))
+		if(document.getElementsByClassName("periodic-table").length>0){
+			window.location.hash="#"+getElement(value).name
+			document.getElementById(getElement(value).name).style.color="red"
+			setTimeout(function(){
+				document.getElementById(getElement(value).name).style.color=""
+			},1000)
+		}
+	}
+}
+function getCompoundDemo(){
+	var value=document.getElementById("getCompound").value
+	if(value!=""){
+		alert(JSON.stringify(getCompound(value)))
+	}
+}
+document.getElementById("getElement").onclick=document.getElementById("getCompound").onclick=function(){
+	this.value=""
+}
+document.getElementById("getElement").onkeydown=function(e){
+	if(e.keyCode==13){
+		getElementDemo()
+	}
+}
+document.getElementById("getCompound").onkeydown=function(e){
+	if(e.keyCode==13){
+		getCompoundDemo()
+	}
+}
